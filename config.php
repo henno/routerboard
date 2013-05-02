ca8ba0db7b39044769b4ba34978c729326627abd
Karmen <karmen.kukk@khk.ee>
2013-05-02 11:33:59 +0300

    First commit.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_DATABASE', 'halo');
+define('DEFAULT_CONTROLLER', 'welcome');
+define('BASE_URL', '/halo/');
+define('ASSETS_URL', '/halo/assets/');
